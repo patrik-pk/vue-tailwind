c16b3389905f20af1f112ddc93033316563b8927
Patrik Pukanec <patrik.pukanec@gmail.com>
2024-10-23 18:35:58 +0200

    tailwind setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="main-container">main</div>
+  <div class="main-container">
+    <div class="text-center bg-blue-500 text-white p-6">
+      <h1 class="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
