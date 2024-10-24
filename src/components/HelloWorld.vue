03a0d6106be01f7fc889906b3c4f1cd67ea31907
Patrik Pukanec <patrik.pukanec@tescan.com>
2024-10-24 11:29:40 +0200

    pattern

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,9 @@
 <template>
-  <div :class="customContainerClass" class="py-8 min-h-screen w-full flex flex-col justify-content-center bg-gradient-to-r from-gradient1 to-gradient2 overflow-hidden">
+  <div :class="customContainerClass" class="relative py-8 min-h-screen w-full flex flex-col justify-content-center bg-gradient-to-r from-gradient1 to-gradient2 overflow-hidden">
+    <div class="absolute top-0 left-0 w-80 h-full">
+      <Pattern />
+    </div>
+
     <div class="flex flex-col">
       <div class="text-white font-semibold text-base bg-primary w-fit rounded-md py-1 px-2">Vaše klientela</div>
       <h1 class="text-white font-semibold text-4xl leading-tight mt-4 lg:text-6xl font-jakarta">Více zákazníků <br> Větší tržby</h1>
@@ -23,11 +27,13 @@
 
 <script setup lang="ts">
 import { customContainerClass } from '../helpers'
+import Pattern from './Pattern.vue'
 
 // TODO: 
 // - tag has opacity
 // - refactor colors
 // - dont forget pattern
+// - fix gradient to match design
 // - button component
 // - img max width unset globally
 // - layout responsivity on desktop
